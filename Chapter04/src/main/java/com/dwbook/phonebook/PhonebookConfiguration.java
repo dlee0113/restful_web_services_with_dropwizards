9003b0da2b7552d888c7a06b966fad79bb5c737d
dlee0113 <dlee0113@gmail.com>
2015-08-27 10:47:23 -0400

    error fix

--- Chapter04/src/main/java/com/dwbook/phonebook/PhonebookConfiguration.java
+++ Chapter04/src/main/java/com/dwbook/phonebook/PhonebookConfiguration.java
@@ -22,30 +22,6 @@ public class PhonebookConfiguration extends Configuration {
     public int getMessageRepetitions() {
         return messageRepetitions;
     }
-package com.dwbook.phonebook;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.dropwizard.Configuration;
-import javax.validation.constraints.Max;
-import org.hibernate.validator.constraints.NotEmpty;
-
-public class PhonebookConfiguration extends Configuration {
-
-    @JsonProperty
-    @NotEmpty
-    private String message;
-
-    @JsonProperty
-    @Max(10)
-    private int messageRepetitions;
-
-    public String getMessage() {
-        return message;
-    }
-
-    public int getMessageRepetitions() {
-        return messageRepetitions;
-    }
 
     @JsonProperty
     private String additionalMessage = "This is optional";
@@ -53,5 +29,4 @@ public class PhonebookConfiguration extends Configuration {
     public String getAdditionalMessage() {
         return additionalMessage;
     }
-}
 }
